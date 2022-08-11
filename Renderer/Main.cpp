301a9f4afac8335c0472a9e4c932450f3ba0393d
keviin0 <kevinhu229@gmail.com>
2022-08-11 12:54:35 -0700

    first commit

---
+++ Renderer/Main.cpp
@@ -0,0 +1,115 @@
+// Main.cpp
+
+#include "tgaimage.h"
+#include "tgaimage.cpp"
+#include "model.h"
+#include "model.cpp"
+#include "geometry.h"
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0  , 0  , 255);
+const TGAColor green = TGAColor(0,   255, 0  , 255);
+
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color);
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color);
+
+int main(int argc, char** argv)
+{
+    Model model("./african_head.obj");
+    TGAImage image(200, 200, TGAImage::RGB);
+
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white); 
+    triangle(t2[0], t2[1], t2[2], image, green);
+    
+    /*
+    for (int i = 0; i < model.nfaces(); i++)
+    {
+        std::vector<int> face = model.face(i);
+        for (int j = 0; j < 3; j++)
+        {
+            Vec3 v0 = model.vert(face[j]);
+            Vec3 v1 = model.vert(face[(j + 1) % 3]);
+            int p0.x = (v0.x + 1.) * image.get_width() / 2.;
+            int p0.y = (v0.y + 1.) * image.get_height() / 2.;
+            int p1.x = (v1.x + 1.) * image.get_width() / 2.;
+            int p1.y = (v1.y + 1.) * image.get_height() / 2.;
+            line(p0.x, p0.y, p1.x, p1.y, image, white);
+        }
+    }
+    */
+    image.flip_vertically(); // Convention is usually that top left is (0, 0) so flipping vertically sets bottom left to being origin
+    image.write_tga_file("output.tga");
+
+    return 0;
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+{
+    // Sort y-values of each vertex in ascending order in order to create segments that will allow us to fill lines later
+    if (t0.y > t1.y) { std::swap(t0, t1); };
+    if (t0.y > t2.y) { std::swap(t0, t2); };
+    if (t1.y > t2.y) { std::swap(t1, t2); };
+
+
+
+
+
+
+
+
+
+    /* Demo to create outlines of triangles and show boundaries
+    line(t0, t1, image, green); // Drawing a line from the coordinate with the smallest y coord to the middle y coord. 
+                                // This forms the bottom segment/half of the triangle which allows lines to be drawn horizontally between the final line and this line.
+    line(t1, t2, image, green); // Upper segment of triangle
+    line(t2, t0, image, red); // Final boundary of triangle which to start the line filling from
+    */
+}
+
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color)
+{
+
+    bool steep = false;
+
+    if (std::abs(p0.y - p1.y) > std::abs(p0.x - p1.x)) // If the line is steep we transpose the image so instead of making sure that every column has one dot, we make sure every row has one dot
+    {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
+        steep = true;
+    }
+
+    if (p0.x > p1.x) // Make sure that a line can also be drawn in reverse
+    {
+        std::swap(p0.x, p1.x);
+        std::swap(p0.y, p1.y);
+    }
+    
+    int dy = p1.y - p0.y;
+    int dx = p1.x - p0.x;
+    float derror = std::abs(dy/float(dx));
+    float error = 0;
+    int y = p0.y;
+    int yinc = (p1.y > p0.y ? 1: -1);
+
+    for (int x = p0.x; x <= p1.x; x++) {
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+        error += derror;
+        if (error > .5)
+        {
+            y += yinc;
+            error -= 1;
+        }
+    }
+}
